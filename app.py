85577bff9e503f0ebd7b136de24f72f2996cbab4
Bromic Ouma <oumabromic7@gmail.com>
2026-02-26 06:48:00 +0300

    Update app.py

--- app.py
+++ app.py
@@ -126,7 +126,7 @@ if st.session_state.is_running and target_input:
                 "TREND": trend_html,
                 "OVERALL RANK": f"#{current_rank}",
                 "VIEWERS": f"{result['viewers']:,}",
-                "LOCATION": f"Page {result['page']}, Position {result['pos']} trend_html",
+                "LOCATION": f"Page {result['page']}, Position {result['pos']} ",trend_html
                 "RAW_RANK": current_rank
             }
             st.session_state.history.insert(0, entry)
